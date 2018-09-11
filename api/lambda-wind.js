1d6ea7f3f52bf7c4de39b66880d274865f9ee154
Craig Gunson <craig.gunson@tabcorp.com.au>
2018-09-11 10:46:58 +1000

    with user agent

--- api/lambda-wind.js
+++ api/lambda-wind.js
@@ -1,19 +1,24 @@
 
-exports.handler = (event, context, callback) => {
-
 process.env.TZ = 'Australia/Sydney'
 var http = require('http');
 var data = '';
 var payload = [];
-var fawkner = "";
-var southchannelisland = "http://www.bom.gov.au/fwo/IDV60901/IDV60901.94853.json";
 
-http.get(southchannelisland, function(res) {
+const options = {
+  hostname: 'www.bom.gov.au',
+  path: '/fwo/IDV60901/IDV60901.94853.json',
+  headers: { 'User-Agent': 'Mozilla/5.0' }
+};
+
+exports.handler = (event, context, callback) => {
+
+
+
+http.get(options, function(res) {
     if (res.statusCode >= 200 && res.statusCode < 400) {
       res.on('data', function(data_) { data += data_.toString(); });
       res.on('end', function() {
         var proc = JSON.parse(data);
-
 
         for (i = 0; i < 5; i++) {
            var when = proc.observations.data[i].local_date_time;
@@ -21,10 +26,7 @@ http.get(southchannelisland, function(res) {
            var gustspeed = proc.observations.data[i].gust_kt;
            var winddir = proc.observations.data[i].wind_dir;
            payload.push(when,windspeed,gustspeed,winddir);
-
            console.log ('when',when,'speed',windspeed,'gust',gustspeed,'winddir',winddir);
-
-
           }
           var payloadjson = JSON.stringify(payload)
   console.log ('payload',payloadjson);
